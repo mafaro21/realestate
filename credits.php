0e5f8b53d860375b61a04ba05d29b693fd53dae8
mafaro21 <mafaro2105@gmail.com>
2021-07-06 17:36:44 +0800

    test

--- credits.php
+++ credits.php
@@ -4,14 +4,15 @@
 <body>
     <?php include 'include/navbar.php' ?>
     <div class="top container ">
-        <p><span>Photo by <a href="https://unsplash.com/@annwallace?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Ann Wallace</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></p>
-        <p><span>Photo by <a href="https://unsplash.com/@octoberroses?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Aubrey Odom</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></p>
-        <p><span>Photo by <a href="https://unsplash.com/@churchoftodd?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">todd kent</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></p>
-        <p><span>Photo by <a href="https://unsplash.com/@micah_carlson?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Micah Carlson</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></p>
-        <p><span>Photo by <a href="https://unsplash.com/@walking_abby?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Abbilyn Zavgorodniaia</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></p>
-        <p><span>Photo by <a href="https://unsplash.com/@michaelwb?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Michael Browning</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></p>
-        <p><span>Photo by <a href="https://unsplash.com/@franagain?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">fran hogan</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></p>
-        <p><span>Photo by <a href="https://unsplash.com/@naomish?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Naomi Hébert</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></p>
+        <p><span>Photo by <a href="https://unsplash.com/@annwallace?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target="_blank">Ann Wallace</a>
+        <p><span>Photo by <a href="https://unsplash.com/@octoberroses?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target="_blank">Aubrey Odom</a>
+        <p><span>Photo by <a href="https://unsplash.com/@churchoftodd?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target="_blank">Todd Kent</a>
+        <p><span>Photo by <a href="https://unsplash.com/@micah_carlson?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target="_blank">Micah Carlson</a>
+        <p><span>Photo by <a href="https://unsplash.com/@walking_abby?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target="_blank">Abbilyn Zavgorodniaia</a>
+        <p><span>Photo by <a href="https://unsplash.com/@michaelwb?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target="_blank">Michael Browning</a>
+        <p><span>Photo by <a href="https://unsplash.com/@franagain?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target="_blank">Fran Hogan</a>
+        <p><span>Photo by <a href="https://unsplash.com/@naomish?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target="_blank">Naomi Hébert</a>
+        <p>All images were from <a href="https://unsplash.com" target="_blank">Unsplash.com</a></p>
 
     </div>
 
